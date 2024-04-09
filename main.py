0c98e8442e207743aa3317d6704c54809ee27223
hermessil <Silas.Hermes02@gmail.com>
2024-04-09 19:51:57 +0200

    setup input routine

--- main.py
+++ main.py
@@ -87,8 +87,8 @@ def cidr_to_subnet_mask(cidr:int) -> str:
 
 
 def check_valid_cidr(input_cidr) -> bool:
-    if len(input_cidr) == 3:
-        if int(input_cidr[1:]) not in range(0, 32):
+    if len(input_cidr) == 2:
+        if int(input_cidr) not in range(0, 32):
             print("Input SubnetMask is not in range of 0-32!")
             return False
         else:
@@ -99,9 +99,9 @@ def check_valid_cidr(input_cidr) -> bool:
         return False
 
 
-def check_valid_ip(ip_address) -> bool:
+def check_valid_ip(ip_addr) -> bool:
     # separate the octets to arr with 4 entries
-    arr_octet = ip_address.split(".")
+    arr_octet = ip_addr.split(".")
     if len(arr_octet) != 4:
         print("The IP-address does not have 4 octets. Please enter valid IP-Address and run again!")
         return False
@@ -114,17 +114,17 @@ def check_valid_ip(ip_address) -> bool:
 
     # Class A Net between 10.0.0.0 and 10.255.255.255
     if int(arr_octet[0]) == 10:
-        print(f"Class A-Net (between 10.0.0.0 and 10.255.255.255) Currently {ip_address}")
+        print(f"Class A-Net (between 10.0.0.0 and 10.255.255.255) Currently {ip_addr}")
         return True
 
     # Class B Net between 172.16.0.0 and 172.31.255.255
     elif int(arr_octet[0]) == 172 and int(arr_octet[1]) < 32 and int(arr_octet[0]) == 172 and int(arr_octet[1]) > 15:
-        print(f"Class B-Net (between 172.16.0.0 and 172.31.255.255) Currently {ip_address}")
+        print(f"Class B-Net (between 172.16.0.0 and 172.31.255.255) Currently {ip_addr}")
         return True
 
     # Class C Net between 192.168.0.0 and 192.168.255.255
     elif int(arr_octet[0]) == 192 and int(arr_octet[1]) == 168:
-        print(f"Class C-Net (between 192.168.0.0 and 192.168.255.255) Currently {ip_address}")
+        print(f"Class C-Net (between 192.168.0.0 and 192.168.255.255) Currently {ip_addr}")
         return True
 
     else:
@@ -133,6 +133,27 @@ def check_valid_ip(ip_address) -> bool:
 
 
 if __name__ == '__main__':
-    #ip_addr = "10.10.1.2"
-    #sn_mask = "255.255.0.0"
-    print(cidr_to_subnet_mask(31))
+    # setting flags
+    valid_ip, valid_cidr = False, False
+    # loop as long as ip or sn mask are not valid
+    while not valid_ip or not valid_cidr:
+        # user input
+        user_input = input("Please enter IP-address in CIDR notation. (Like: '1.2.3.4 /24')\n"
+                           "==>")
+        # check if "/" is in input
+        if "/" in user_input:
+            # split the input to split_input[0] = ip address, split_input[1] = CIDR
+            split_input = user_input.split("/")
+            # loop over all elements to remove spaces
+            for index, elements in enumerate(split_input):
+                # remove all spaces in split_input
+                split_input[index] = split_input[index].replace(" ", "")
+            # assign values to variables
+            ip_address, cidr = split_input[0], split_input[1]
+            # check validation of ip address
+            valid_ip = check_valid_ip(ip_address)
+            # check valid cidr
+            valid_cidr = check_valid_cidr(cidr)
+        else:
+            # print invalid format
+            print("The format is not correct! Please set CIDR with '/' and try again!. ")
